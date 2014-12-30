bfd7d5e1166a8f94e43d59eed2fe5cefcb69df44
Mark Small <m.small@ed.ac.uk>
2014-12-30 23:38:23 +0000

    Test plugin's settings:
    Since the plugin sets some defaults, we now test these
    defaults are set correctly.

--- src/test/groovy/edina/shared/gradle/EdinaPluginTest.groovy
+++ src/test/groovy/edina/shared/gradle/EdinaPluginTest.groovy
@@ -1,17 +1,43 @@
 package edina.shared.gradle
 
+import static org.junit.Assert.assertNotNull
+import static org.junit.Assert.assertTrue
+
+import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Before
 import org.junit.Test
 
 class EdinaPluginTest {
-    
-  @Test
-  public void applyPluginToProject() {
-    Project project = ProjectBuilder.builder().build()
-    project.apply plugin: 'edina'
+	
+  private Project project;
 
-//    assertTrue(project.tasks.hello instanceof GreetingTask)
+  @Before
+  public void setup() {
+    project = ProjectBuilder.builder().build()
+    project.apply plugin: 'edina'
   }
-  
+
+  @Test
+  public void isMavenPluginApplied() {
+    assertNotNull(project.plugins.getPlugin('maven'))
+  }
+
+  @Test
+  public void isJavaPluginApplied() {
+    assertNotNull(project.plugins.getPlugin('java'))
+  }
+
+  @Test
+  public void isSourceCompatible() {
+    def result = project.sourceCompatibility == JavaVersion.VERSION_1_8
+    assertTrue(result)
+  }
+
+  @Test
+  public void hasGeodevRepository() {
+    assertNotNull(project.repositories.getByName('geodev'))
+  }
+
 }
